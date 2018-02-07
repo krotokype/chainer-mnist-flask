7c2887e26644986e88e832b6cbea0a3c504ea406
krotokype <36023293+krotokype@users.noreply.github.com>
2018-02-07 22:29:16 +0900

    Add Web UI

--- server.py
+++ server.py
@@ -24,6 +24,10 @@ serializers.load_npz(npz, model)
 # Create an application
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
 @app.route('/predict', methods=['POST'])
 def predict():
     x = np.array(request.json, dtype=np.float32)
